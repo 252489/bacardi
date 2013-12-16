272e672d0f46ac61a877468315ea462c7205fef7
Pavel Ivanov <pavel.ivanov@corp.mail.ru>
2013-12-16 22:59:48 +0400

    some changes

--- src/GBP/BacardiBundle/Resources/public/js/finish/finish.js
+++ src/GBP/BacardiBundle/Resources/public/js/finish/finish.js
@@ -3,11 +3,9 @@ var Finish = {
     vars: {
         effectsOptions: {
             'desaturate': {
-                vignette: 1,
                 desaturate: true
             },
             'sepia': {
-                vignette: 0.6,
                 sepia: true
             }
         }
@@ -18,18 +16,13 @@ var Finish = {
     methods: {
 
         changeEffect: function () {
-            Finish.elems.$resultLoader.show();
             Finish.elems.$effectBtn.removeClass('active');
             $(this).addClass('active');
 
             var effect = Finish.vars.effectsOptions[$(this).attr('data-effect')];
             new VintageJS(
                 Finish.elems.$resultPhotoImg[0],
-                {
-                    onStop: function () {
-                        Finish.elems.$resultLoader.hide();
-                    }
-                },
+                null,
                 effect
             );
         }
@@ -51,8 +44,7 @@ var Finish = {
 
             $resultFrame:       $('#js-b-finish__result__frame'),
             $resultPhoto:       $('#js-b-finish__result__photo'),
-            $resultPhotoImg:    $('#js-b-finish__result__photo__img'),
-            $resultLoader:      $('#js-b-finish__result__loader')
+            $resultPhotoImg:    $('#js-b-finish__result__photo__img')
         };
 
         this.setMethods();
