968dd39d69c884d9e6a1be8e24d7c7bb52d6eed1
Pavel Ivanov <pavel.ivanov@corp.mail.ru>
2013-12-16 23:22:54 +0400

    some changes

--- src/GBP/BacardiBundle/Resources/public/js/finish/finish.js
+++ src/GBP/BacardiBundle/Resources/public/js/finish/finish.js
@@ -25,6 +25,33 @@ var Finish = {
                 null,
                 effect
             );
+        },
+
+        changeFrame: function () {
+            var id = $(this).parent().index();
+
+            Finish.elems.$resultFrameImg.removeClass('active');
+            Finish.elems.$resultFrameImg.eq(id).addClass('active');
+        },
+
+        getPhotoUrl: function () {
+            var canvas  = $('<canvas></canvas>')[0],
+                photo   = Finish.elems.$resultPhotoImg,
+                frame   = Finish.elems.$resultFrame.find('img:visible');
+
+            canvas.width  = 384;
+            canvas.height = 567;
+
+            canvas.getContext('2d').drawImage(photo[0], 0, 0);
+            canvas.getContext('2d').drawImage(frame[0], 0, 0);
+
+            return canvas.toDataURL();
+        },
+
+        finish: function (isSaveOnPk) {
+            Finish.elems.$imgInput.val(this.getPhotoUrl());
+            Finish.elems.$saveTypeInput.val(isSaveOnPk);
+            Finish.elems.$form.submit();
         }
 
     },
@@ -36,15 +63,38 @@ var Finish = {
             Finish.methods.changeEffect.bind(this)();
         });
 
+        this.elems.$frameBtn.on('click', function () {
+            if ($(this).hasClass('active')) return;
+            Finish.methods.changeFrame.bind(this)();
+        });
+
+        this.elems.$save.on('click', function () {
+        	Finish.methods.finish(1);
+        });
+
+        this.elems.$send.on('click', function () {
+            Finish.methods.finish(0);
+        });
+
     },
 
     init: function () {
         this.elems = {
             $effectBtn:         $('.js-b-finish__style-btn'),
+            $frameBtn:          $('.js-b-finish__frame'),
 
+            $save:              $('#js-b-finish__save'),
+            $send:              $('#js-b-finish__send'),
+
+            $result:            $('#js-b-finish__result'),
             $resultFrame:       $('#js-b-finish__result__frame'),
+            $resultFrameImg:    $('.js-b-finish__result__frame__img'),
             $resultPhoto:       $('#js-b-finish__result__photo'),
-            $resultPhotoImg:    $('#js-b-finish__result__photo__img')
+            $resultPhotoImg:    $('#js-b-finish__result__photo__img'),
+
+            $form:              $('#js-b-finish__form'),
+            $imgInput:          $('#js-img-input'),
+            $saveTypeInput:     $('#js-save-type-input')
         };
 
         this.setMethods();
